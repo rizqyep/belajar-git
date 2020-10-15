eba0c0e460a65898939c2a5a6aef296c88b1cb58
Rizqy EP <rizqyepr@gmail.com>
2020-10-15 20:25:34 +0700

    menambahkan input nama

--- program-simple.c
+++ program-simple.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 
-    printf("Halo github!");
+    char nama[20];
+
+    printf("Masukkan nama :");
+    fgets(nama, 20, stdin);
+    strtok(nama, "\n");
+    printf("Halo %s", nama);
     return 0;
     
 }
